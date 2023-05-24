6c19692e142efd489e973c64a095e13d28bfcf6b
لطفی نیا ، طراحی وب <E.Lotfi@exir.rayaneh>
2023-05-24 11:17:04 +0330

    Update fake-data.ts

--- client/src/app/services/fake-data.ts
+++ client/src/app/services/fake-data.ts
@@ -2,7 +2,7 @@ import * as _mod from 'src/app/models';
 
 export const fakeData: _mod.Quote[] = [
     {
-        "Id": 1,
+        "ID": 1,
         "Content": "This is a sample sentence.",
         "Trans": "این یک جمله نمونه است.",
         "Season": 1,
@@ -12,7 +12,7 @@ export const fakeData: _mod.Quote[] = [
         "CntSeen": 2
     },
     {
-        "Id": 2,
+        "ID": 2,
         "Content": "The quick brown fox jumps over the lazy dog.",
         "Trans": "روباه سریع و قهوه‌ای روی سگ تنبل می‌پرد.",
         "Season": 1,
@@ -22,7 +22,7 @@ export const fakeData: _mod.Quote[] = [
         "CntSeen": 1
     },
     {
-        "Id": 3,
+        "ID": 3,
         "Content": "I am learning how to translate sentences.",
         "Trans": "در حال یادگیری ترجمه جملات هستم.",
         "Season": 1,
@@ -32,7 +32,7 @@ export const fakeData: _mod.Quote[] = [
         "CntSeen": 0
     },
     {
-        "Id": 4,
+        "ID": 4,
         "Content": "The cat is sleeping on the mat.",
         "Trans": "گربه روی فرش خوابیده است.",
         "Season": 2,
@@ -42,7 +42,7 @@ export const fakeData: _mod.Quote[] = [
         "CntSeen": 0
     },
     {
-        "Id": 5,
+        "ID": 5,
         "Content": "My favorite color is blue.",
         "Trans": "رنگ مورد علاقه من آبی است.",
         "Season": 2,
@@ -52,7 +52,7 @@ export const fakeData: _mod.Quote[] = [
         "CntSeen": 0
     },
     {
-        "Id": 6,
+        "ID": 6,
         "Content": "I love to read books in my free time.",
         "Trans": "من عاشق خواندن کتاب در وقت آزادم هستم.",
         "Season": 2,
@@ -62,7 +62,7 @@ export const fakeData: _mod.Quote[] = [
         "CntSeen": 1
     },
     {
-        "Id": 7,
+        "ID": 7,
         "Content": "He went to the store and bought some milk.",
         "Trans": "او به فروشگاه رفت و شیرخرید.",
         "Season": 3,
@@ -72,7 +72,7 @@ export const fakeData: _mod.Quote[] = [
         "CntSeen": 3
     },
     {
-        "Id": 8,
+        "ID": 8,
         "Content": "The sun rises in the east and sets in the west.",
         "Trans": "خورشید در شرق طلوع می‌کند و در غرب غروب می‌کند.",
         "Season": 3,
@@ -82,7 +82,7 @@ export const fakeData: _mod.Quote[] = [
         "CntSeen": 2
     },
     {
-        "Id": 9,
+        "ID": 9,
         "Content": "The exam was difficult, but I passed.",
         "Trans": "امتحان سخت بود، اما من قبول شدم.",
         "Season": 3,
@@ -92,7 +92,7 @@ export const fakeData: _mod.Quote[] = [
         "CntSeen": 0
     },
     {
-        "Id": 10,
+        "ID": 10,
         "Content": "My favorite movie is The Shawshank Redemption.",
         "Trans": "فیلم مورد علاقه من از زندان شاوشنک است.",
         "Season": 4,
